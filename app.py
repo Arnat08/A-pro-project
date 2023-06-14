31f6fcc884c19acdfae7b273133027026e6c61c3
Arnat08 <124722999+Arnat08@users.noreply.github.com>
2023-06-15 00:38:44 +0600

    weather added and login fixed

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, render_template, session, redirect,url_for
-from models import db,Users,Tasks
+from functools import wraps
+from Weather import weather
+from flask import Flask, request, render_template, session, redirect, url_for
+from models import db, Users, Tasks
 from config import Configuration
+import json
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -8,10 +12,10 @@ db.init_app(app)
 
 
 def login_required(route):
+    @wraps(route)
     def decorated_route(*args, **kwargs):
-        if not session.get('username'):
+        if 'username' not in session:
             return redirect('/login')
-
         return route(*args, **kwargs)
 
     return decorated_route
@@ -19,29 +23,71 @@ def login_required(route):
 
 @app.route('/', methods=['GET'])
 def main_page():
-    return render_template('main.html')     #главная страница
+    return render_template('main.html')  # главная страница
 
 
-@app.route('/register', methods=['GET', 'POST'])
+def login_not_required(route):
+    @wraps(route)
+    def decorated_route(*args, **kwargs):
+        if 'username' in session:
+            return redirect(url_for('task'))
+        return route(*args, **kwargs)
+
+    return decorated_route
+
+
+def registration_validation(userdata):
+    user = Users.query.filter_by(username=userdata['username']).first()
+    email = Users.query.filter_by(email=userdata['email']).first()
+    # if userdata['password'] != user.password:
+    #     return True
+    if user is not None:
+        return True
+    if email is not None:
+        return True
+
+
+@app.route('/register', methods=["GET", "POST"])
+@login_not_required
 def register():
-    if request.method == 'GET':
-        return render_template('auth/register.html')
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        confirm_password = request.form.get('confirmPassword')
-        print(username,email,password)
+    if request.method == "POST":
+        if registration_validation(request.form):
+            return render_template('auth/register.html.html', title='Страница регистрации пользователя')
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
         user = Users(username=username, email=email, password=password)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        session['username'] = username
+        return redirect(url_for('task'))
+    return render_template('auth/register.html', title='Страница регистрации пользователя')
 
 
+# @app.route('/register', methods=['GET', 'POST'])
+# @login_not_required
+# def register():
+#     if request.method == 'GET':
+#         return render_template('auth/register.html')
+#     elif request.method == 'POST':
+#         username = request.form.get('username')
+#         email = request.form.get('email')
+#         password = request.form.get('password')
+#         confirm_password = request.form.get('confirmPassword')
+#         #print(username, email, password)
+#         user = Users(username=username, email=email, password=password)
+#         db.session.add(user)
+#         db.session.commit()
+#         session['username'] = username
+#         return redirect(url_for('login'))  # Перенаправляем на страницу входа
+#     return render_template('auth/register.html')  # Исправленный путь к шаблону
 
-@app.route('/task', methods=['GET', 'POST'])
-def logout():
-    return render_template('art.html')
+
+@app.route('/task', methods=['GET'])
+def task():
+    x = weather()
+    username = session.get('username')
+    return render_template('auth/to-do-page.html', username=username, temp=x.json['current']['temp_c'],city=x.json['location']['name'])
 
 
 # Добавил проверку логина и пароля
@@ -50,8 +96,9 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        user = Users.query.filter_by(username=username).first()
-        if user and user.password == password:
+        user = Users.query.filter_by(username=username, password=password).first()
+        print(user.password, user.username)
+        if user.password == password:
             session['username'] = username
             return redirect(url_for('task'))
         else:
@@ -60,18 +107,12 @@ def login():
     return render_template('auth/login.html')
 
 
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+    session.pop('username')
+    return redirect(url_for('login'))
 
-
-# @app.route('/to-do-page', methods=['GET', 'POST'])
-# def to_do_page():
-#     return render_template('to-do-page.html')
-#
-# @app.route('/logout', methods=['GET', 'POST'])
-# def logout():
-#     return render_template('logout.html')
-#
-
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run()
     with app.app_context():
         db.create_all()
--- models.py
+++ models.py
@@ -1,20 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
-db= SQLAlchemy()
+
+db = SQLAlchemy()
+
 
 class Users(db.Model):
-    #__tablename__ = "users"
+    # __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(200))
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     tasks = db.relationship('Tasks', lazy=True)
 
 
-
 class Tasks(db.Model):
-   # __tablename__ = 'tasks'
+    # __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     status = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
