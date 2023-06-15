e8f07d3e3e24e7174fd1ce60b64bf390ec0de3e9
Әшімбаев Арнат <ashimbayev_a@cpi.nis.edu.kz>
2023-06-15 12:13:58 +0600

    login and registrar fixed

--- Weather.py
+++ Weather.py
@@ -6,7 +6,7 @@ from config import open_weather_token
 def weather():
     api_url = 'https://weatherapi-com.p.rapidapi.com/current.json'
     headers = {'X-RapidAPI-Key': 'e6219e657cmsh92edde1afaa90f1p18a47ejsn1d3554ded7f9'}
-    params = {'q': 'Almaty'}
+    params = {'q': 'Astana'}
     responce = requests.get(api_url, headers=headers, params=params)
     if responce.ok:
         weather_data = responce.json()
